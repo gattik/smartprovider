47a43e202241080a31e358822a8ac52530cde641
Kat Gatt <kgatti@ryerson.ca>
2013-08-13 20:40:06 +0000

    Empty App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smartprovider::Application.config.secret_key_base = '50fcebb3abe9a9e3b128a6804cc9a391fc6eb4885d4b210fcc178850700e5bf1d04c86710680ba6ad28d1b5473eb6479e1a7f182f961ef92c884d1cba2db1bf3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Smartprovider::Application.config.session_store :cookie_store, key: '_smartprovider_session'
