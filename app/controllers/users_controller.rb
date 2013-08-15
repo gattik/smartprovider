89483c4c85f9ef67b67c8fa42f7adb207b3efa43
Kat Gatt <kgatti@ryerson.ca>
2013-08-15 03:05:00 +0000

    tests pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,24 +9,34 @@ class UsersController < ApplicationController
 
   def create
   	@user = User.new(user_params)
+  	@user.save
+
+  	redirect_to users_path
   end
 
   def show
+  	@user = User.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  	@user = User.find(params[:id])
+  	@user.update(user_params)
+  	redirect_to users_path
   end
 
   def destroy
+  	@user = User.find(params[:id])
+  	@user.destroy
+  	redirect_to users_path
   end
 
 private
 
   def user_params
-  	params.require(:user).permit(:first_name, :last_name, )
+  	params.require(:user).permit(:first_name, :last_name, :designation, :phone, :email, :password, :password_confirmation)
 
 
   end
