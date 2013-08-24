27ece9d8dc9ecf2578553bb5ec2b9472e4228b49
ronathan <ronaldvictorino@gmail.com>
2013-08-24 04:33:24 +0000

    Added validation and test to ensure vitals are not left empty

--- app/models/vital.rb
+++ app/models/vital.rb
@@ -1,3 +1,11 @@
 class Vital < ActiveRecord::Base
 	belongs_to :client
+
+	validate :any_present?
+
+	def any_present?
+	  if weight.empty? && height.empty? && systolic.empty? && diastolic.empty? && beats.empty? && blood_sugar.empty? && breaths.empty? && temperature.empty?
+	    errors.add :base, "All fields cannot be blank."
+	  end
+	end
 end
--- test/models/vital_test.rb
+++ test/models/vital_test.rb
@@ -2,4 +2,18 @@ require 'test_helper'
 
 class VitalTest < ActiveSupport::TestCase
 
+	test "should not allow vitals to save if all fields are empty" do
+		vital = Vital.new(weight: "", 
+			height: "",
+			systolic: "",
+			diastolic: "",
+			beats: "",
+			blood_sugar: "",
+			breaths: "",
+			temperature: "")
+
+		assert vital.invalid?
+		assert vital.errors.any?
+	end
+
 end
