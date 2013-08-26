40b6f41468ce29b01915eeaf6ca7a9899b8b16a7
ronathan <ronaldvictorino@gmail.com>
2013-08-26 21:11:03 +0000

    Removed javascripts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,16 +14,10 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-      respond_to do |format|
-        UserMailer.welcome_email(@user).deliver
-        format.html { redirect_to login_path }
-        format.js 
-      end
+      UserMailer.welcome_email(@user).deliver
+      redirect_to login_path
     else
-      respond_to do |format|
-        format.html { render action: :new }
-        format.js
-      end
+      render action: :new
     end
   end
 
