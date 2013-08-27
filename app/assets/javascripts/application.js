1105f4177ece3786389047b0df242ad09f3160f9
gattik <kgatti@ryerson.ca>
2013-08-26 22:52:17 -0400

    map on dashboard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,16 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+var menu = document.querySelector('.menu');
+var anchors = menu.getElementsByTagName('a');
+
+for (var i = 0; i < anchors.length; i += 1) {
+  anchors[i].addEventListener('click', function() { clickHandler(anchors[i]) }, false);
+}
+
+function clickHandler(anchor) {
+  var hasClass = anchor.getAttribute('class');
+  if (hasClass !== 'active') {
+    anchor.setAttribute('class', 'active');
+  }
+}
