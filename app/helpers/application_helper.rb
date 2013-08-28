88aeca281d91e1bd9f36af0f2a272d755e18d313
gattik <kgatti@ryerson.ca>
2013-08-28 00:27:24 -0400

    views, avatar, users show, banners

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   def avatar_url(user)
     default_url = "#{root_url}images/guest.png"
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=30&d=#{CGI.escape(default_url)}"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=200&d=#{CGI.escape(default_url)}"
   end
 end
