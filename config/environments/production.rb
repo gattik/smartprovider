9a53358d82f41864eb963c558717d2e3915b86da
ronathan <ronaldvictorino@gmail.com>
2013-09-04 18:36:20 +0000

    Added mailers to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,16 @@ Smartprovider::Application.configure do
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mandrillapp.com",
+    port: 587,
+    user_name: "ronaldvictorino@gmail.com",
+    password: "mlPTR7setBN4hfTjQ224XQ",
+    authentication: "login",
+    enable_starttls_auto: true
+  }
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
